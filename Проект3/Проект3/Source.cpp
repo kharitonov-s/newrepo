5c8ec7d33b0cba39d3e7928be43b57ab0f3185e0
ss1488 <bdizzz322@gmail.com>
2015-10-02 15:46:37 +0300

    Update Source.cpp

--- Проект3/Проект3/Source.cpp
+++ Проект3/Проект3/Source.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 	double  a1, a2, b1, b2, c1, c2, x, y;
-	cout << "Введите коэфициенты уравнений системы: А1x+B1y+C1=0 и A2x+B2y+C2=0" << endl;
-	cin >> a1 >> a2 >> b1 >> b2 >> c1 >> c2 >> endl;
-	x=
-
+	cout << "vvedite koeficienti:" << endl;
+	cin >> a1 >> a2 >> b1 >> b2 >> c1 >> c2;
+	y = ((a2*c1) - (a1*c2)) / ((b2*a1) - (a2*b1));
+	x = ((-b2*y) - c2) / a2;
+	cout << "x=" << x << "y=" << y << endl;
 	system("pause");
 	return 0;
 
